a76854c95e3657535edd91621c3fbc6c4a3d8f12
Alex Rosca <alex@rosca.xyz>
2026-03-27 23:08:20 +0100

    Add Firefox for integration with eID

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ dnf -y copr enable scottames/ghostty
 dnf -y install ghostty
 
 rpm -i https://eid.belgium.be/sites/default/files/software/eid-archive-fedora-2026-1.noarch.rpm
-dnf -y install eid-viewer eid-mw
+dnf -y install firefox eid-viewer eid-mw
 
 # Disable COPRs so they don't end up enabled on the final image:
 dnf -y copr disable scottames/ghostty
