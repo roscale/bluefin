e73f0141d0ad414d3aad583efb6ecc2de45053b0
Alex Rosca <alex@rosca.xyz>
2026-03-27 23:02:12 +0100

    Add Belgian eID

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,8 @@ set -ouex pipefail
 dnf -y copr enable scottames/ghostty
 dnf -y install ghostty
 
+rpm -i https://eid.belgium.be/sites/default/files/software/eid-archive-fedora-2026-1.noarch.rpm
+dnf -y install eid-viewer eid-mw
+
 # Disable COPRs so they don't end up enabled on the final image:
 dnf -y copr disable scottames/ghostty
